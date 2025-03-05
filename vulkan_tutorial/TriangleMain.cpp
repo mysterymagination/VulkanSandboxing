735a2ae41671698d8706d2c6d047d045864f4706
Jeff Creswell <creswel2@gmail.com>
2025-03-05 16:33:02 -0500

    triangling

--- vulkan_tutorial/TriangleMain.cpp
+++ vulkan_tutorial/TriangleMain.cpp
@@ -41,6 +41,22 @@ private:
         appInfo.pEngineName = "No Engine";
         appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
         appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount = 0;
+        const char **glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        // no val layers for now, but if we had gt 0 we would populate struct prop ppEnabledLayerNames.
+        createInfo.enabledLayerCount = 0;
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
+        {
+            throw std::runtime_error("failed to create triangly vkinstance");
+        }
     }
 
     void initVulkan()
