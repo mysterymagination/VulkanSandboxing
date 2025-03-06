e55b9ad13b7b4d69d13541ab0285aa4ed94f7802
Jeff Creswell <creswel2@gmail.com>
2025-03-05 20:01:12 -0500

    layering it up

--- vulkan_tutorial/TriangleMain.cpp
+++ vulkan_tutorial/TriangleMain.cpp
@@ -9,6 +9,14 @@
 
 const uint32_t WINDOW_WIDTH = 800;
 const uint32_t WINDOW_HEIGHT = 600;
+const std::vector<const char *> validationLayers = {
+    "VK_LAYER_KHRONOS_validation"};
+
+#ifdef NDEBUG
+const bool enableValidationLayers = false;
+#else
+const bool enableValidationLayers = true;
+#endif
 
 class HelloTriangleApplication
 {
@@ -36,6 +44,18 @@ private:
 
     void createInstance()
     {
+        // I'm having a disagreement with the tutorial on how best to handle the C/C++ interop since Vulkan is a C API but our program is C++... well anyway, it's easier to build up wrappers around pure data than to decompose wrappers into the appropriate data, so let's go with cstring constant and then build a wrapper of wrappers from that why not. Technically we're creating an opportunity for a disconnect between what layers we check for and what we actually request, but I don't wanna use strcmp and friends in the analysis. I don't wanna!
+        std::vector<std::string> validationLayerStrings;
+        for (auto cstring : validationLayers)
+        {
+            validationLayerStrings.emplace_back(std::string(cstring));
+        }
+
+        if (enableValidationLayers && !checkValidationLayerSupport(validationLayerStrings))
+        {
+            throw std::runtime_error("some desired validation layers are not available!");
+        }
+
         VkApplicationInfo appInfo{};
         appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
         appInfo.pApplicationName = "Triangular Sandboxing";
@@ -54,8 +74,15 @@ private:
         glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
         createInfo.enabledExtensionCount = glfwExtensionCount;
         createInfo.ppEnabledExtensionNames = glfwExtensions;
-        // no val layers for now, but if we had gt 0 we would populate struct prop ppEnabledLayerNames.
-        createInfo.enabledLayerCount = 0;
+        if (enableValidationLayers)
+        {
+            createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+            createInfo.ppEnabledLayerNames = validationLayers.data();
+        }
+        else
+        {
+            createInfo.enabledLayerCount = 0;
+        }
 
         if (!checkExtensions(std::vector<std::string>(glfwExtensions, glfwExtensions + glfwExtensionCount)))
         {
@@ -69,10 +96,48 @@ private:
     }
 
     /**
+     * @brief checks if the input validation layers are supported by the driver.
+     * @input reference to a string vector of desired validation layers.
+     * @return true if all desired layers are supported; false otherwise.
+     */
+    bool checkValidationLayerSupport(const std::vector<std::string> &desiredValidationLayers)
+    {
+        uint32_t layerCount;
+        vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+
+        std::vector<VkLayerProperties> availableLayers(layerCount);
+        vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+
+        std::cout << "available val layers:\n";
+        for (const auto &layer : availableLayers)
+        {
+            std::cout << '\t' << layer.layerName << '\n';
+        }
+
+        bool allDesiredLayersAreSupported = true;
+        for (const auto &desiredLayer : desiredValidationLayers)
+        {
+            const auto it = std::find_if(availableLayers.begin(), availableLayers.end(), [desiredLayer](VkLayerProperties layerProps)
+                                         { return layerProps.layerName == desiredLayer; });
+            allDesiredLayersAreSupported = allDesiredLayersAreSupported && it != availableLayers.end();
+            if (allDesiredLayersAreSupported)
+            {
+                std::cout << "layer " << desiredLayer << " is supported." << '\n';
+            }
+            else
+            {
+                std::cerr << "layer " << desiredLayer << " is NOT supported." << '\n';
+            }
+        }
+        return allDesiredLayersAreSupported;
+    }
+
+    /**
      * @brief Enumerates the supported vk extensions on stdout and checks them against the input required extension set.
+     * @param reference to a vector of strings identifying the required extension names.
      * @return true if all the required extensions are also found in the supported extensions set; false otherwise.
      */
-    bool checkExtensions(const std::vector<std::string> requiredExtensions)
+    bool checkExtensions(const std::vector<std::string> &requiredExtensions)
     {
         // check which vk extensions the driver supports against our requirements.
         uint32_t supportedExtensionCount = 0;
