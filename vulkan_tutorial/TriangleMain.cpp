e01040bb5418458e35783aae98d5a75e5cbea388
Jeff Creswell <creswel2@gmail.com>
2025-03-05 19:06:11 -0500

    extensioning

--- vulkan_tutorial/TriangleMain.cpp
+++ vulkan_tutorial/TriangleMain.cpp
@@ -4,6 +4,8 @@
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
+#include <vector>
+#include <algorithm>
 
 const uint32_t WINDOW_WIDTH = 800;
 const uint32_t WINDOW_HEIGHT = 600;
@@ -46,6 +48,7 @@ private:
         createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
         createInfo.pApplicationInfo = &appInfo;
 
+        // figure out how many and which vk extensions GLFW requires.
         uint32_t glfwExtensionCount = 0;
         const char **glfwExtensions;
         glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
@@ -53,10 +56,55 @@ private:
         createInfo.ppEnabledExtensionNames = glfwExtensions;
         // no val layers for now, but if we had gt 0 we would populate struct prop ppEnabledLayerNames.
         createInfo.enabledLayerCount = 0;
+
+        if (!checkExtensions(std::vector<std::string>(glfwExtensions, glfwExtensions + glfwExtensionCount)))
+        {
+            throw std::runtime_error("failed to create triangly vkinstance due to unsupported extensions");
+        }
+
         if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS)
         {
             throw std::runtime_error("failed to create triangly vkinstance");
         }
+    }
+
+    /**
+     * @brief Enumerates the supported vk extensions on stdout and checks them against the input required extension set.
+     * @return true if all the required extensions are also found in the supported extensions set; false otherwise.
+     */
+    bool checkExtensions(const std::vector<std::string> requiredExtensions)
+    {
+        // check which vk extensions the driver supports against our requirements.
+        uint32_t supportedExtensionCount = 0;
+        // call in once to find out how many vk extensions the driver supports.
+        vkEnumerateInstanceExtensionProperties(nullptr, &supportedExtensionCount, nullptr);
+        // init vector of extension prop structs to the count of supported extensions we just learned.
+        std::vector<VkExtensionProperties> supportedExtensions(supportedExtensionCount);
+        // call in again to populate the supported extensions via vector's C data interface.
+        vkEnumerateInstanceExtensionProperties(nullptr, &supportedExtensionCount, supportedExtensions.data());
+
+        std::cout << "available extensions:\n";
+        for (const auto &extension : supportedExtensions)
+        {
+            std::cout << '\t' << extension.extensionName << '\n';
+        }
+
+        bool allRequiredExtensionsAreSupported = true;
+        for (const auto &requiredExtension : requiredExtensions)
+        {
+            const auto it = std::find_if(supportedExtensions.begin(), supportedExtensions.end(), [requiredExtension](VkExtensionProperties props)
+                                         { return props.extensionName == requiredExtension; });
+            allRequiredExtensionsAreSupported = allRequiredExtensionsAreSupported && it != supportedExtensions.end();
+            if (allRequiredExtensionsAreSupported)
+            {
+                std::cout << "extension " << requiredExtension << " is supported." << '\n';
+            }
+            else
+            {
+                std::cerr << "extension " << requiredExtension << " is NOT supported." << '\n';
+            }
+        }
+        return allRequiredExtensionsAreSupported;
     }
 
     void initVulkan()
@@ -74,6 +122,7 @@ private:
 
     void cleanup()
     {
+        vkDestroyInstance(instance, nullptr);
         glfwDestroyWindow(window);
         glfwTerminate();
     }
